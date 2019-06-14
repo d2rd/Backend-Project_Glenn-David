1f4c4471c03767efa1612eaab4f99ad936d1b9f3
David Daniel <d2rd@outlook.com>
2019-06-13 20:59:20 -0700

    Branch for TypeError and compile failure.

--- notes/src/components/notes/Notes.js
+++ notes/src/components/notes/Notes.js
@@ -155,7 +155,7 @@ deleteNoteHandler7 = _id => {
           </div>
           <div className="Notes-panel">
             <ul>
-              {this.state.notes.map((note, _id) =>
+              {this.state.notes.map((note, _id) => {
                 <li key={_id}>
                 <div className="Note-title-row"><h3 className="Title">{note.title}</h3><h3 className="Price">Price: ${note.price}</h3><h3 className="Priority">Priority: {note.priority}</h3>
                 </div>
@@ -170,9 +170,8 @@ deleteNoteHandler7 = _id => {
                   {/* <Button className="btn-itemDelete" onClick={()=> this.props.removeNote(note._id)}>Delete-A</Button> */}
                   <h5>{note._id}</h5>
                 </li>
+                }
 // ☞ 04a1312d-1275-4a50-8c41-bf1867af6999
-
-
               )}
             </ul>
           </div>
